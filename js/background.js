33c25d4b86dc904ab71470837d0cb82dbf5f5928
jianrry <jianrry@gmail.com>
2019-08-21 23:19:31 +0800

    添加“知乎”搜索引擎

--- js/background.js
+++ js/background.js
@@ -1,19 +1,10 @@
+//工具类
 chrome.contextMenus.create({
     title: "使用维基百科（中文）搜索 '%s'", 
     contexts: ['selection'], 
     onclick: function(params)
     {
         chrome.tabs.create({url: 'https://zh.wikipedia.org/wiki/' + encodeURI(params.selectionText)});
-    }
-});
-
-
-chrome.contextMenus.create({
-    title: "使用GitHub搜索：'%s'", 
-    contexts: ['selection'], 
-    onclick: function(params)
-    {
-        chrome.tabs.create({url: 'https://github.com/search?q=' + encodeURI(params.selectionText)});
     }
 });
 
@@ -26,6 +17,29 @@ chrome.contextMenus.create({
         chrome.tabs.create({url: 'https://store.steampowered.com/account/registerkey?key=' + encodeURI(params.selectionText)});
     }
 });
+
+
+//社区类
+chrome.contextMenus.create({
+    title: "使用GitHub搜索：'%s'", 
+    contexts: ['selection'], 
+    onclick: function(params)
+    {
+        chrome.tabs.create({url: 'https://github.com/search?q=' + encodeURI(params.selectionText)});
+    }
+});
+
+chrome.contextMenus.create({
+    title: "使用知乎搜索：'%s'", 
+    contexts: ['selection'], 
+    onclick: function(params)
+    {
+        chrome.tabs.create({url: 'https://www.zhihu.com/search?q=' + encodeURI(params.selectionText)});
+    }
+});
+
+
+
 
 
 
